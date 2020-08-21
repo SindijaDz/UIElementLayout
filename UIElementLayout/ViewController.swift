db21aa11c0016eee50383f8bea7e0e68d1507ba1
SindijaDz <sindijad@yahoo.com>
2020-08-21 19:58:24 +0300

    Initial Commit

---
+++ UIElementLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIElementLayout
+//
+//  Created by sindija.dzintare on 21/08/2020.
+//  Copyright © 2020 sindija.dzintare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
